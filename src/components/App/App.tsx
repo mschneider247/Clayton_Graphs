d942e78a1d4f1d40aeb663f78a6408f8dfdc0a1a
Michael <mschneider247@gmail.com>
2021-07-12 14:19:22 -0600

    Add Filter component with placeholder buttons

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from '../Header/Header'
+import { Filter } from '../Filter/Filter'
 import './App.css';
 
 const App = () => {
   return (
     <main>
       <Header />
+      <Filter />
     </main>
   );
 }
