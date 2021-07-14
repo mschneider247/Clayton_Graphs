5ba84479567feab50cd4068a8ea711ee2d2dac2b
Michael <mschneider247@gmail.com>
2021-07-13 18:52:21 -0600

    Create mock data for miles run

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,8 +2,10 @@ import { Header } from '../Header/Header'
 import { Filter } from '../Filter/Filter'
 import { GraphCollection } from '../GraphCollection/GraphCollection'
 import './App.css';
+import { milesRun } from '../../testData/testData'
 
 const App = () => {
+  console.log("milesRun??", milesRun)
   return (
     <main>
       <Header />
---
+++ src/testData/testData.tsx
@@ -0,0 +1,32 @@
+const milesRun = {
+  2020: {
+    jan: 123,
+    feb: 345,
+    mar: 183,
+    apr: 132,
+    may: 161,
+    jun: 342,
+    jul: 154,
+    aug: 123,
+    sep: 320,
+    oct: 234,
+    nov: 693,
+    dec: 214,
+  },
+  2021 : {
+    jan: 123,
+    feb: 345,
+    mar: 783,
+    apr: 432,
+    may: 534,
+    jun: 342,
+    jul: 354,
+    aug: 123,
+    sep: 320,
+    oct: 234,
+    nov: 693,
+    dec: 234,
+  },
+}
+
+export { milesRun }
