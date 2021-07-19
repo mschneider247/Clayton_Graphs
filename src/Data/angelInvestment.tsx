8fba6a3c6064afe26991967224f665137bd89c87
Michael <mschneider247@gmail.com>
2021-07-18 20:00:43 -0600

    Add initial angelInvestment data

---
+++ src/Data/angelInvestment.tsx
@@ -0,0 +1,32 @@
+const angelInvestment = {
+  2020: {
+    jan: 1,
+    feb: 2,
+    mar: 0,
+    apr: 3,
+    may: 5,
+    jun: 0,
+    jul: 1,
+    aug: 6,
+    sep: 3,
+    oct: 2,
+    nov: 1,
+    dec: 5,
+  },
+  2021: {
+    jan: 4,
+    feb: 1,
+    mar: 6,
+    apr: 5,
+    may: 2,
+    jun: 5,
+    jul: 1,
+    aug: '',
+    sep: '',
+    oct: '',
+    nov: '',
+    dec: '',
+  },
+}
+
+export { angelInvestment }
