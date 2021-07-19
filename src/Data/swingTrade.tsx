634ee20e5e7926b38735ef4f31210a0c753145d5
Michael <mschneider247@gmail.com>
2021-07-19 11:59:51 -0600

    Add initial swingTrade data

---
+++ src/Data/swingTrade.tsx
@@ -0,0 +1,32 @@
+const swingTrade = {
+  2020: {
+    jan: [6, 3],
+    feb: [8, 6],
+    mar: [3, 4],
+    apr: [6, 1],
+    may: [4, 8],
+    jun: [9, 3],
+    jul: [4, 5],
+    aug: [2, 5],
+    sep: [2, 9],
+    oct: [4, 1],
+    nov: [8, 1],
+    dec: [2, 2],
+  },
+  2021: {
+    jan: [12, 4],
+    feb: [6, 5],
+    mar: [1, 4],
+    apr: [2, 7],
+    may: [5, 4],
+    jun: [5, 6],
+    jul: [7, 3],
+    aug: [],
+    sep: [],
+    oct: [],
+    nov: [],
+    dec: [],
+  },
+}
+
+export { swingTrades }
