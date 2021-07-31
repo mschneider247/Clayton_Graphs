d3574f59a4ed6a2fb5f455bfc06d9afd9967306c
Michael <mschneider247@gmail.com>
2021-07-30 19:08:38 -0600

    Add image for Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,18 @@
 import './Header.css';
 
 const Header = () => {
+
+  let titleImage = `images/claytonGraph.jpeg`;
+  console.log(titleImage)
+
   return (
     <header className="main_title">
       <h1>Clayton Graphs!</h1>
+      <img 
+        className="title_image"
+        alt="Clayton Graph" 
+        src={titleImage}
+      />
     </header>
   )
 }
